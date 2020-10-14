399f96d6ee4c85aacc418a626b4b3163ae3ba31e
GrubeG <gggrube@uwalumni.com>
2020-10-13 23:41:22 -0500

    Update basemaps

--- js/geojson.js
+++ js/geojson.js
@@ -5,19 +5,25 @@ function createMap(){
     
     //add OSM base tilelayer
     var CartoDB_Positron = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
-	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-	subdomains: 'abcd',
-	maxZoom: 19
+	   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+	   subdomains: 'abcd',
+	   maxZoom: 19
 });
-        
-    var Esri_NatGeoWorldMap = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}', {
-	attribution: 'Tiles &copy; Esri &mdash; National Geographic, Esri, DeLorme, NAVTEQ, UNEP-WCMC, USGS, NASA, ESA, METI, NRCAN, GEBCO, NOAA, iPC',
-	maxZoom: 16
+    //add Esri tilelayer    
+    var Esri_NatGeoWorldMap =           L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}', {
+	   attribution: 'Tiles &copy; Esri &mdash; National Geographic, Esri, DeLorme, NAVTEQ, UNEP-WCMC, USGS, NASA, ESA, METI, NRCAN, GEBCO, NOAA, iPC',
+	   maxZoom: 16
+});
+    
+    var OpenStreetMap_Mapnik = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	maxZoom: 19,
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 });
        
     var baseMaps = {
-    "Topo": Esri_NatGeoWorldMap,
-    "Black and White": CartoDB_Positron
+    "Basic": CartoDB_Positron,
+    "Topographic": Esri_NatGeoWorldMap,
+    "Streets": OpenStreetMap_Mapnik
     };
     
     //create the map
